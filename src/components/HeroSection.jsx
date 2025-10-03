cf06762d17ea00e30f19528e8fa1393a2348ba36
Mudassar Manzoor <mohsanbhatti864@gmail.com>
2025-10-03 12:10:18 +0500

    update banner

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,39 +5,33 @@ import { Link } from "react-router-dom";
 const HeroSection = () => {
   return (
     <section className="">
-      <div className="flex justify-between items-center max-w-7xl mx-auto px-5  min-h-[300px] gap-10 overflow-hidden">
-        
+      <div className="flex justify-between items-center w-full mx-auto min-h-[300px]  relative overflow-hidden">
         {/* Left Column (60%) */}
-        <div className="w-[50%] z-10 flex flex-col ">
-          <Link to="/">
-          <img
-            style={{ width: "120px" }}
-            src="./ghp-logo.png"
-            alt="Logo"
-            className="mb-12"
-          />
+        <div className="w-[calc(100%-650px)] z-10 flex flex-col px-4 py-10  ">
+          <Link to="/" className="max-w-[120px] py-2">
+            <img
+              style={{ width: "120px" }}
+              src="./ghp-logo.png"
+              alt="Logo"
+              className="mb-12"
+            />
           </Link>
-          <h1 className="text-6xl font-bold leading-tight relative text-right text-[#635a55]">
-            Vacci<span className="text-[#e37a23]">NATION</span>
-          </h1>
-          <span className="text-[clamp(1.2rem,3vw,1.8rem)] block leading-snug mt-4 text-right">
-            Comparing vaccination systems globally
-          </span>
-          <img
-            src={syringe}
-            alt="syringe"
-            className="w-[300px] h-auto -mt-2 rotate-180 self-end"
-          />
+          <div className="flex justify-end flex-col">
+            <h1 className="text-6xl font-bold leading-tight text-right text-[#635a55]">
+              Vacci<span className="text-[#e37a23]">NATION</span>
+            </h1>
+            <span className="text-3xl block leading-snug mt-4 text-right">
+              Comparing vaccination systems globally
+            </span>
+            <img
+              src={syringe}
+              alt="syringe"
+              className="w-[300px] h-auto -mt-2 rotate-180 self-end"
+            />
+          </div>
+          <img src={global} alt="global" className="w-[800px] h-auto absolute -top-[60%] -right-28 -z-10 " />
         </div>
-        
-        {/* Right Column (40%) */}
-        <div className="w-[50%] flex justify-center">
-          <img
-            src={global}
-            alt="Hero"
-            className="w-full h-auto -mt-40 -ml-20 "
-          />
-        </div>
+
       </div>
     </section>
   );
